e4ff80f4007b951d45bacbb73f78d8771832eebc
rittmang <ritomgupta99@gmail.com>
2020-09-20 18:41:08 +0530

    First commit

---
+++ public/js/main.js
@@ -0,0 +1,126 @@
+/*
+	Fractal by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-mobile');
+		else {
+
+			breakpoints.on('>medium', function() {
+				$body.removeClass('is-mobile');
+			});
+
+			breakpoints.on('<=medium', function() {
+				$body.addClass('is-mobile');
+			});
+			var $banner = $('#banner');
+
+			if ($banner.length > 0) {
+
+				// IE fix.
+					if (skel.vars.IEVersion < 12) {
+
+						$window.on('resize', function() {
+
+							var wh = $window.height() * 0.60,
+								bh = $banner.height();
+
+							$banner.css('height', 'auto');
+
+							window.setTimeout(function() {
+
+								if (bh < wh)
+									$banner.css('height', wh + 'px');
+
+							}, 0);
+
+						});
+
+						$window.on('load', function() {
+							$window.triggerHandler('resize');
+						});
+
+					}
+
+				// Video check.
+					var video = $banner.data('video');
+
+					if (video)
+						$window.on('load.banner', function() {
+
+							// Disable banner load event (so it doesn't fire again).
+								$window.off('load.banner');
+
+							// Append video if supported.
+								if (!skel.vars.mobile
+								&&	!skel.breakpoint('large').active
+								&&	skel.vars.IEVersion > 9)
+									$banner.append('<video autoplay loop><source src="' + video + '.mp4" type="video/mp4" /><source src="' + video + '.webm" type="video/webm" /></video>');
+
+						});
+
+				// More button.
+					$banner.find('.more')
+						.addClass('scrolly');
+
+			}
+
+		}
+
+	// Scrolly.
+		$('.scrolly')
+			.scrolly({
+				speed: 1500
+			});
+			// Poptrox.
+			$window.on('load', function() {
+
+				var $thumbs = $('.thumbnails');
+
+				if ($thumbs.length > 0)
+					$thumbs.poptrox({
+						onPopupClose: function() { $body.removeClass('is-covered'); },
+						onPopupOpen: function() { $body.addClass('is-covered'); },
+						baseZIndex: 10001,
+						useBodyOverflow: false,
+						overlayColor: '#222226',
+						overlayOpacity: 0.75,
+						popupLoaderText: '',
+						fadeSpeed: 500,
+						usePopupDefaultStyling: false,
+						windowMargin: (skel.breakpoint('small').active ? 5 : 50)
+					});
+
+			});
+
+		// Initial scroll.
+			$window.on('load', function() {
+				$window.trigger('scroll');
+			});
+
+})(jQuery);
---
+++ resources/views/index.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>JEE Material</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="css/main.css" />
+		<noscript><link rel="stylesheet" href="css/noscript.css" /></noscript>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-161216442-1"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			
+			gtag('config', 'UA-161216442-1');
+		</script>
+
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<div class="content">
+					<h1>Shared - JEE Material</h1>
+					<p>Your favourite site for JEE documents<br />
+					simplified!</p>
+					<ul class="actions">
+						<li><a href="https://drive.google.com/open?id=1pcwAvCGYpo-rO1p1E9EbHjYuuyzdHHoP" target="_blank" class="button primary icon fa-folder-open">Visit</a></li>
+						<li><a href="#one" class="button icon fa-chevron-down scrolly">Learn More</a></li>
+					</ul>
+					<ul class="actions">
+						<li><a href="store" target="_blank" class="button icon fa-shopping-cart">Store</a></li>
+					</ul>	
+				</div>
+				<div class="image phone"><div class="inner"><img src="images/screen.jpg" alt="" /></div></div>
+			</header>
+
+
+			<section id="one" class="wrapper">
+				<header class="major">
+					<h2><strong>Content added</strong><br />
+					on 20<sup>th</sup> May, 2019</h2>
+				</header>
+				<div class="inner alt">
+					
+					<section class="special">
+						<ul class="icons labeled">
+							<li><span class="icon fa-file-pdf-o"><span class="label">Allen tests under <strong>Allen</strong></span></span></li><br />
+							<li><span class="icon fa-file-pdf-o"><span class="label">FIIT-JEE tests under <strong>FIITJEE-2  → New</strong></span></span></li><br />
+							<li><span class="icon fa-file-pdf-o"><span class="label">Resonance tests under <strong>Resonance  → New</strong></span></span></li><br />
+						</ul>
+					</section>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper style2 special">
+				<header class="major">
+					<h2>A <strong>voluntary and free</strong> service<br />
+					for JEE aspirants</h2>
+				</header>
+				<div class="inner alt">
+					
+					<section class="special">
+						<ul class="icons labeled">
+							<li><span class="icon fa-refresh"><span class="label">Updated regularly</span></span></li>
+							<li><span class="icon fa-ban"><span class="label">No ads. Period.</span></span></li>
+							<li><span class="icon fa-heart"><span class="label">Made with love.</span></span></li>
+							
+						</ul>
+					</section>
+				</div>
+			</section>
+
+
+		<!-- Three -->
+			<section id="three" class="wrapper style2 special">
+				<header class="major">
+					<h2>We're hearing!</h2>
+					<p>Wish to tell us something? Think we're doing good?<br />Send in your feedback, or shoot a mail. We'll try to respond as soon as we can.</p>
+				</header>
+				<ul class="actions special">
+					<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfqSZKYwanxpgkuSjP_lmWmoF6o9eRo6Rq5luYVnlqbUQK_Hg/viewform" target="_blank" class="button primary icon fa-external-link">Feedback</a></li>
+					<li><a href="mailto:rightonrittman@gmail.com" class="button icon fa-envelope">Mail</a></li>
+				</ul>
+			</section>
+
+		
+        <!-- Footer -->
+            @include('footer')
+			
+		<!-- Scripts -->
+			<script src="js/jquery.min.js"></script>
+			<script src="js/jquery.scrolly.min.js"></script>
+			<script src="js/browser.min.js"></script>
+			<script src="js/breakpoints.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
+
+	</body>
+</html>
