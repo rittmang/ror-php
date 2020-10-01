2dec625810495a0c697f3e03823739e79e836bf8
rittmang <ritomgupta99@gmail.com>
2020-10-01 22:11:35 +0530

    Add ToS notice

---
+++ resources/views/movies/noticealert.blade.php
@@ -0,0 +1,8 @@
+<div class="alert text-center cookiealert" role="alert" style="font-size:small">
+    <b>Important!</b>This content is made available for experimental purposes, and is not meant to be shared. Your device information is processed on-device, and only an anonymized unique character sequence is delivered to us for updating view count. <a href="tos" target="_blank">Learn more</a>
+
+    <button type="button" class="btn btn-primary btn-sm acceptcookies">
+        I agree
+    </button>
+</div>
+<script src="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.js"></script>
