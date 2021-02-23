6abedab95ffee976026cd788f14e3845acdafa39
rittmang <ritomgupta99@gmail.com>
2021-02-23 15:15:17 +0530

    Latest first

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -17,7 +17,7 @@ class MovieController extends Controller
         $upcoming_titles=explode(',',env('UPCOMING_TITLES'));
         $banner_titles=explode(',',env('BANNER_TITLES'));
         $upcoming_movielist=DB::table('title')->select('id','name','long_poster','age','duration')->whereIn('id',$upcoming_titles)->get();
-        $banner_movielist=DB::table('title')->orderBy('id','asc')->select('id','name','type','wide_poster','age','duration','description','trailer_link')->whereIn('id',$banner_titles)->get();
+        $banner_movielist=DB::table('title')->orderBy('id','desc')->select('id','name','type','wide_poster','age','duration','description','trailer_link')->whereIn('id',$banner_titles)->get();
         return view('movies/index',['upcoming_titles'=>$upcoming_movielist,'banner_titles'=>$banner_movielist]);
     }
     public function allMovies(){
