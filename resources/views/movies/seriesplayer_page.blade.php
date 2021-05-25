97a30e3b36c5f1ea470e0b50dee88be51eb94fbd
rittmang <ritomgupta99@gmail.com>
2021-05-25 22:04:13 +0530

    Add episode name

--- resources/views/movies/seriesplayer_page.blade.php
+++ resources/views/movies/seriesplayer_page.blade.php
@@ -20,7 +20,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>S{{ $ep->season }}E{{ $ep->episode }} | {{ $title->name }}</title>
+    <title>S{{ $ep->season }}E{{ $ep->episode }} | {{$ep->ep_name}} | {{ $title->name }}</title>
     <!-- Favicon -->
     <link rel="shortcut icon" href="../../../../movie/images/favicon.ico" />
     <!-- Bootstrap CSS -->
