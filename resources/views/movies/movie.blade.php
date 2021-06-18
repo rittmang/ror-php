0becfb35a43d3f8c9d314a615f93cb73330ac8cf
rittmang <ritomgupta99@gmail.com>
2021-06-18 12:42:43 +0530

    remove go up

--- resources/views/movies/movie.blade.php
+++ resources/views/movies/movie.blade.php
@@ -266,9 +266,6 @@
      </footer>
     <!-- MainContent End-->
     <!-- back-to-top -->
-    <div id="back-to-top">
-        <a class="top" href="#top" id="top"> <i class="fa fa-angle-up"></i> </a>
-    </div>
 
     <!-- back-to-top End -->
     <!-- jQuery, Popper JS -->
